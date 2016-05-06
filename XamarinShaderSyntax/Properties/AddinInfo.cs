4eb68f6d904855027ae5c8078d03493610041039
SushiHangover <sushihangover@outlook.com>
2016-05-06 06:43:04 -0700

    first commit

---
+++ XamarinShaderSyntax/Properties/AddinInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+	"SushiHangover.Shader.Syntax",
+	Namespace = "SushiHangoverShaderSyntax",
+	Version = "1.0"
+)]
+
+[assembly: AddinName("SushiHangover.Shader.Syntax")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("Adds Shader Mimetype")]
+[assembly: AddinAuthor("SushiHangover/RobertN")]
+
